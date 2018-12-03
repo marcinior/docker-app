2c6b3966ddebbd8f592e5c055505b03cd8494c25
Marcin Bobel <marcin.bobel7@gmail.com>
2018-12-03 18:10:25 +0100

    Update mysql.java

--- mysql.java
+++ mysql.java
@@ -2,20 +2,6 @@ import java.sql.*;
 import jva.util.Scanner;
 
 class MyClass {
-	public static void main(String args[]) {/*Statement statement;
-        Connection conn = null;
-
-        try {
-
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/myDB", "root", null
-            );
-
-            Scanner in = new Scanner(System.in);
-            while(true) {
-               
-        } catch(Exception e) {
-            System.out.println(e);
-        }*/
+	public static void main(String args[]) {
+        }
     }
